deaa0621e8d59f0e75f49ba50f70360604ba7d73
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2011-06-25 19:45:18 +0200

    Add C# expression eval function to ProfileLib and SetupEnv function

--- Documents/WindowsPowerShell/ProfileLib.ps1
+++ Documents/WindowsPowerShell/ProfileLib.ps1
@@ -1,2 +1,19 @@
+Function cs($expr) {
+	$ClassName = "Test" + (Get-Random)
+	Add-Type -TypeDefinition @"
+using System;
+
+public class $ClassName { 
+	public void test() { 
+		Console.WriteLine($expr);
+	}
+}
+"@
+	(New-Object $ClassName).test()
+}
+
+Function SetupEnv() {
+    [Environment]::SetEnvironmentVariable("GIT_SSH", "plink", "User")
+}
 
 Set-Alias vi gvim
