a17783ea26cdf36da550772a013b59dbf26cc46a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2011-06-25 19:59:56 +0200

    Add AppendUserPath to ProfileLib. Append vim and putty paths.

--- Documents/WindowsPowerShell/ProfileLib.ps1
+++ Documents/WindowsPowerShell/ProfileLib.ps1
@@ -12,7 +12,25 @@ public class $ClassName {
 	(New-Object $ClassName).test()
 }
 
+Function AppendUserPath($path) {
+    if (-not (Test-Path $path)) {
+        Write-Error "AppendUserPath: $path does not exist!"
+        return
+    }
+    $oldpath = [Environment]::GetEnvironmentVariable("PATH", "User")
+    if ($oldpath -ne $null) {
+        if ($oldpath.Contains($path)) {
+            $path = $oldpath
+        } else {
+            $path = $oldpath + ";" + $path
+        }
+    }
+    [Environment]::SetEnvironmentVariable("PATH", $path, "User")
+}
+
 Function SetupEnv() {
+    AppendUserPath 'C:\Program Files (x86)\Putty'
+    AppendUserPath 'C:\Program Files (x86)\Vim\vim73'
     [Environment]::SetEnvironmentVariable("GIT_SSH", "plink", "User")
 }
 
